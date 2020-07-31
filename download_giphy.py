40baab80e8d4bc92673ff848478cd5c5a6f10767
Xinyue Zhang (Amber) <43156916+XinyueZhang831@users.noreply.github.com>
2020-07-31 15:29:22 -0700

    Add files via upload

--- download_giphy.py
+++ download_giphy.py
@@ -1,33 +1,44 @@
 import os
 import pandas as pd
 import requests
+import time
 
 
-def clean_df():
-    df = pd.read_csv('./giphy.csv',converters={'image_tag': eval})
+#def clean_df():
+    #df = pd.read_csv('./giphy.csv',converters={'image_tag': eval})
 
 
-    for i,r in df.iterrows():
-        main_part = 'https://i.giphy.com/'
-        middle_part = ('/').join(r['image_url'].split('/')[4:5])
-        last = '.gif'
-        d_url = main_part+middle_part+last
-        df.at[i, 'dowload_url'] = d_url
+    #for i,r in df.iterrows():
+        #main_part = 'https://i.giphy.com/'
+        #middle_part = ('/').join(r['image_url'].split('/')[4:5])
+        #last = '.gif'
+        #d_url = main_part+middle_part+last
+        #df.at[i, 'dowload_url'] = d_url
         #add tag here
-        tag_list = ['animal', 'cartoon', 'bunny', 'dog', 'cat','anime', 'simpson','spongebob', 'simpsons']
-        for eachtag in tag_list:
-            for each_tag in r['image_tag']:
-                if eachtag in each_tag:
-                    df.at[i,'download'] = 'No'
-        df.to_csv('download_prep.csv')
+        #tag_list = ['animal', 'cartoon', 'bunny', 'dog', 'cat','anime', 'simpson','spongebob', 'simpsons']
+        #print(i)
+        #for eachtag in tag_list:
+            #for each_tag in r['image_tag']:
+                #if eachtag in each_tag:
+                    #df.at[i,'download'] = 'No'
+        #df.to_csv('download_prep.csv')
 
 
 def download_gif():
 
-    df = pd.read_csv('./download_prep.csv')
+    df = pd.read_csv('./giphy.csv')
 
     for i,r in df.iterrows():
-        if r['download'] != 'No':
-            with open(os.getcwd() + '/' + str(r['Unnamed: 0']) + '.gif', 'wb') as f:
-                f.write(requests.get(r['dowload_url']).content)
+        #if r['download'] != 'No':
+        # windows dir format: 'D:\/your_folder1\/folder2'+'\/'+ str(r['Unnamed: 0']) + '.gif', 'wb'
+        main_part = 'https://i.giphy.com/'
+        middle_part = ('/').join(r['image_url'].split('/')[4:5])
+        last = '.gif'
+        link = main_part+middle_part+last
+        with open(os.getcwd() + '/' + str(r['Unnamed: 0']) + '.gif', 'wb') as f:
+            f.write(requests.get(link).content)
+            print('finish:' + str(i))
+        # time.sleep(20000)
 
+
+download_gif()
